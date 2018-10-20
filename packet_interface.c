152e0ec4ed70d32deca9837239b65257a70c16da
PierreLambert3 <pierre@localhost.localdomain>
2018-10-20 17:18:34 +0200

    ichangements encode/decode

--- packet_interface.c
+++ packet_interface.c
@@ -29,13 +29,12 @@ struct __attribute__((__packed__)) pkt {
 	uint32_t crc2;
 };
 
-dans le pdf : numerotation des bits different!!
 
 /* Extra code */
 /* Your code will be inserted here */
 
 pkt_t* pkt_new(){
-	pkt_t* pkt_ptr = calloc(sizeof(struct pkt), 0);
+	pkt_t* pkt_ptr = calloc(1, sizeof(struct pkt)); //changement ici (avant = size, 0)
 	return pkt_ptr;
 }
 	
@@ -69,36 +68,27 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt){
 	unsigned int trFlag;
 	unsigned int window; 
 	setTypeTrWinFromData(data, &type, &trFlag, &window);
-	
-	uint8_t seqNum = *(data + 1);
+	// memcpy(pkt, data, sizeof(uint8_t));
+	uint8_t seqNum;
+	memcpy(&seqNum, data+1, sizeof(uint8_t));
+	///uint8_t seqNum = *(data + 1);
 	
 	uint16_t length;
-	memcpy(&length, data+2, 2);
+	memcpy(&length, data+2, sizeof(uint16_t));
 	length = ntohs(length);
 	
 	uint32_t timestamp;
-	memcpy(&timestamp, data+4, 4);
-	timestamp = ntohl(timestamp);
+	memcpy(&timestamp, data+4, sizeof(uint32_t));
+	/// timestamp = ntohl(timestamp);
+
+
 	
 	uint32_t crc1Received;
-	memcpy(&crc1Received, data+8, 4);
-	crc1Received = ntohl(crc1Received);
-	
-	pkt_set_type(pkt, type);
-	pkt_set_tr(pkt, 0); // tr = 0 pour crc
-	pkt_set_window(pkt, window);
-	pkt_set_seqnum(pkt, seqNum);
-	pkt_set_length(pkt, length);
-	pkt_set_timestamp(pkt, timestamp);
-
-	uLong crc1Computed = crc32(0L, Z_NULL, 0);
-	crc1Computed = crc32(crc1Computed, (const Bytef *)pkt, 8);
-	
-	pkt_set_tr(pkt, trFlag); // tr was 0 for crc
-	if (crc1Computed != crc1Received){
-		return E_CRC;
+	memcpy(&crc1Received, data+8, sizeof(uint32_t));
+	crc1Received = ntohl(crc1Received);	
+	if (trFlag==1 && type!=PTYPE_DATA){
+		return E_TR;
 	}
-	pkt_set_crc1(pkt, crc1Received);
 	if (type == 0){
 		return E_TYPE;
 	}
@@ -106,23 +96,48 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt){
 		if (len != 12){
 			return E_LENGTH;
 		}
-	}else{
+	}
+
+	pkt_set_type(pkt, type);
+	pkt_set_tr(pkt, 0); // tr = 0 pour crc
+	pkt_set_window(pkt, window);
+	pkt_set_seqnum(pkt, seqNum);
+	pkt_set_length(pkt, length);
+	pkt_set_timestamp(pkt, timestamp);
+
+	/// uLong crc1Computed = crc32(0L, Z_NULL, 0);
+	uint32_t crc1Computed = 0;
+	crc1Computed = crc32(crc1Computed, (Bytef *)pkt, sizeof(uint64_t));
+
+
+	pkt_set_tr(pkt, trFlag); // tr was 0 for crc
+	if (crc1Computed != ntohl(crc1Received)){
+		return E_CRC;
+	}
+
+	pkt_set_crc1(pkt, crc1Received);
+
+
+	if(length != 0){
 		if (len != 12 + (size_t)length + 4){
 			return E_LENGTH;
 		}
 		uint32_t crc2Received;
-		memcpy(&crc2Received, data+12+length, 4);
+
+		memcpy(&crc2Received, data+12+length, sizeof(uint32_t));
 		crc2Received = ntohl(crc2Received);
-		uLong crc2Computed = crc32(0L, Z_NULL, 0);
+		///uLong crc2Computed = crc32(0L, Z_NULL, 0);
+		uint32_t crc2Computed = 0;
 		crc2Computed = crc32(crc2Computed, (const Bytef *)data+12, length);
 		if (crc2Received != crc2Computed){
 			return E_CRC;
 		}
-		pkt_set_crc2(pkt, crc2Computed);
+
+		pkt_set_crc2(pkt, crc2Received);
 		char* payload = malloc(length); 
 		memcpy(payload, data+12, length);	
 		pkt_set_payload(pkt, payload, length);
-		printf("%u\n\n", pkt_get_timestamp(pkt));
+
 	}
 	return PKT_OK;
 }
@@ -133,14 +148,31 @@ pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len){
 	if(totalSize > *len){ //buffer trop petit
 		return E_NOMEM;
 	}
-	int copyLength = 1 + 1 + 2 + 4 + 4; //8bits + seqNum(8bit) + length(16bit) + timeStamp(32bit) + crc32(32bit)
+
+	int copyLength = 1 + 1 + 2;/// + 4;/// + 4; //8bits + seqNum(8bit) + length(16bit) + timeStamp(32bit) + crc32(32bit)
+	uint32_t time = pkt_get_timestamp(pkt);
+	// time = htonl(time);
 	memcpy(buf, pkt, copyLength);
+	memcpy(buf+copyLength, &time, sizeof(uint32_t));
+	uint32_t crc1 = 0;
+	char * header = malloc(sizeof(uint64_t));
+	memcpy(header, buf, sizeof(uint64_t));
+	*header = *header & 0b11011111;
+	crc1 = crc32(crc1, (Bytef *)header, sizeof(uint64_t));
+	crc1 = htonl(crc1);
+	crc1 = pkt_get_crc1(pkt);
+	/// printf("AAAAA%u    %u\n", crc1, pkt_get_crc1(pkt));
+	memcpy(buf+copyLength+sizeof(uint32_t), &crc1, sizeof(uint32_t));
 	if(payLength == 0){ //no payload
 		return PKT_OK;
 	}
 	// has a payload
-	memcpy(buf + copyLength, pkt->payload, payLength);
-	memcpy(buf + copyLength + payLength, &pkt->crc2, 4);
+	memcpy(buf + copyLength + 2*sizeof(uint32_t), pkt_get_payload(pkt), payLength);
+	uint32_t crc2 = 0;
+	crc2 = crc32(crc2, (Bytef *)pkt->payload, sizeof(char)*payLength);
+	crc2 = htonl(crc2);
+	memcpy(buf+copyLength+payLength + 2*sizeof(uint32_t), &crc2, sizeof(uint32_t));
+	// memcpy(buf + copyLength + payLength, &pkt->crc2, 4);
 	return PKT_OK;
 }
 
@@ -202,6 +234,11 @@ pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type){
 }
 
 pkt_status_code pkt_set_tr(pkt_t *pkt, const uint8_t tr){
+	
+	if ((pkt->type!= PTYPE_DATA)&&(tr==1)){
+		return E_TR;
+	}	
+
 	if(tr > 0b1){
 		return E_TR;
 	}
@@ -232,6 +269,9 @@ pkt_status_code pkt_set_seqnum(pkt_t *pkt, const uint8_t seqnum){
 }
 
 pkt_status_code pkt_set_length(pkt_t *pkt, const uint16_t length){
+	if (length>512)	{
+		return E_LENGTH;
+	}
 	if(pkt != NULL){
 		pkt->length = htons(length);
 		return PKT_OK;
@@ -270,7 +310,7 @@ pkt_status_code pkt_set_payload(pkt_t *pkt,
 		if(pkt->payload != NULL){
 			free(pkt->payload);
 		}
-		pkt->payload = malloc(length);
+		pkt->payload = malloc(length*sizeof(char));
 		memcpy(pkt->payload, data, length);
 		pkt_set_length(pkt, length);
 		return PKT_OK;
@@ -278,63 +318,67 @@ pkt_status_code pkt_set_payload(pkt_t *pkt,
 	return E_UNCONSISTENT;
 }
 
-int main(){
-	pkt_t* paquet;
-	paquet = pkt_new();
+// int main(){
+// 	pkt_t* paquet;
+// 	paquet = pkt_new();
 
-	// printf("setters...\n");
-	pkt_status_code typeRet = pkt_set_type(paquet, 0b11);
-	pkt_status_code trRet = pkt_set_tr(paquet, 0b0);
-	pkt_status_code winRet = pkt_set_window(paquet, 0b11111);
-	pkt_status_code seqRet = pkt_set_seqnum(paquet, 0b01000000);
-	pkt_status_code lenRet = pkt_set_length(paquet, 0b0000100);
-	pkt_status_code timeRet = pkt_set_timestamp(paquet, 0b01111111111111111111111111111110);
-	printf("tr:%u\n",pkt_get_tr(paquet));
-	printf("window:%u\n",pkt_get_window(paquet));
-	printf("seqNum:%u\n",pkt_get_seqnum(paquet));
-	printf("length:%u\n",pkt_get_length(paquet));
-	printf("time:%u\n",pkt_get_timestamp(paquet));
-	printf("type:%u\n",pkt_get_type(paquet));
+// 	// printf("setters...\n");
+// 	pkt_status_code typeRet = pkt_set_type(paquet, 0b11);
+// 	pkt_status_code trRet = pkt_set_tr(paquet, 0b0);
+// 	pkt_status_code winRet = pkt_set_window(paquet, 0b11111);
+// 	pkt_status_code seqRet = pkt_set_seqnum(paquet, 0b01000000);
+// 	pkt_status_code lenRet = pkt_set_length(paquet, 0b0000100);
+// 	pkt_status_code timeRet = pkt_set_timestamp(paquet, 0b01111111111111111111111111111110);
+// 	printf("tr:%u\n",pkt_get_tr(paquet));
+// 	printf("window:%u\n",pkt_get_window(paquet));
+// 	printf("seqNum:%u\n",pkt_get_seqnum(paquet));
+// 	printf("time:%u\n",pkt_get_timestamp(paquet));
+// 	printf("type:%u\n",pkt_get_type(paquet));
 
 
-	char* pay = (char*)malloc(8*sizeof(char));
-	strcpy(pay, "hello world");
-	int llen = strlen(pay);
-	pkt_set_payload(paquet, pay, llen);
-	uLong crc1 = crc32(0L, Z_NULL, 0);
-	crc1 = crc32(crc1, (const Bytef *)paquet, 8);
+// 	char* pay = (char*)malloc(8*sizeof(char));
+// 	strcpy(pay, "hello world");
+// 	int llen = strlen(pay);
+// 	pkt_set_payload(paquet, pay, llen);
+// 	printf("length:%u\n",pkt_get_length(paquet));
+// 	uint32_t crc1 = 0;
+// 	crc1 = crc32(crc1, (Bytef *)paquet, sizeof(uint64_t));
 
-	pkt_status_code crc1Ret = pkt_set_crc1(paquet, crc1);
-	printf("crc = %u\n", crc1);
-	uLong crc2 = crc32(0L, Z_NULL, 0);
-	crc2 = crc32(crc2, (const Bytef *)paquet->payload, llen);
-	pkt_status_code crc2Ret = pkt_set_crc2(paquet, crc2);
-	// printf("crc : %u %d", crc1, crc2);
-	ptypes_t type = pkt_get_type(paquet);
-	uint8_t tr = pkt_get_tr(paquet);
-	uint8_t window = pkt_get_window(paquet);
-	uint8_t seqNum = pkt_get_seqnum(paquet);
-	uint16_t length = pkt_get_length(paquet);
-	uint32_t timestamp = pkt_get_timestamp(paquet);
-	uint32_t crcGet = pkt_get_crc1(paquet);
-	const char* payload = pkt_get_payload(paquet);
+// 	// uint32_t crc1Computed = 0;
+// 	// crc1Computed = crc32(crc1Computed, (Bytef *)pkt, sizeof(uint64_t));
 
-	// printf("get type: %u\n", type);
-	// printf("get tr: %u\n", tr);
-	// printf("get window: %u\n", window);
-	// printf("get seqNum: %u\n", seqNum);
-	// printf("get length: %u\n", length);
-	// printf("get timestamp: %u\n", timestamp);
-	printf("get crc1: %u\n", crcGet);
-	// printf("payload : %s\n", payload);
+// 	pkt_status_code crc1Ret = pkt_set_crc1(paquet, crc1);
+// 	printf("crc = %u\n", crc1);
+// 	uint32_t crc2 = 0;
+// 	crc2 = crc32(crc2, (const Bytef *)paquet->payload, llen);
+// 	pkt_status_code crc2Ret = pkt_set_crc2(paquet, crc2);
+// 	// printf("crc : %u %d", crc1, crc2);
+// 	ptypes_t type = pkt_get_type(paquet);
+// 	uint8_t tr = pkt_get_tr(paquet);
+// 	uint8_t window = pkt_get_window(paquet);
+// 	uint8_t seqNum = pkt_get_seqnum(paquet);
+// 	uint16_t length = pkt_get_length(paquet);
+// 	uint32_t timestamp = pkt_get_timestamp(paquet);
+// 	uint32_t crcGet = pkt_get_crc1(paquet);
+// 	const char* payload = pkt_get_payload(paquet);
 
-	// printf("\n");
-	size_t len = llen+16;
-	char* buff = malloc(len*sizeof(char));
-	pkt_encode(paquet, buff, &len);
-	pkt_t* decPakt = pkt_new();
-	pkt_decode(buff, llen+16,decPakt);
-	pkt_del(paquet);
-	pkt_del(decPakt);
-   	return 0;
-}
+// 	// printf("get type: %u\n", type);
+// 	// printf("get tr: %u\n", tr);
+// 	// printf("get window: %u\n", window);
+// 	// printf("get seqNum: %u\n", seqNum);
+// 	// printf("get length: %u\n", length);
+// 	// printf("get timestamp: %u\n", timestamp);
+// 	printf("get crc1: %u\n", crcGet);
+// 	// printf("payload : %s\n", payload);
+
+// 	// printf("\n");
+// 	size_t len = llen+16;
+// 	char* buff = malloc(len*sizeof(char));
+// 	pkt_encode(paquet, buff, &len);
+// 	pkt_t* decPakt = pkt_new();
+// 	pkt_decode(buff, llen+16,decPakt);
+// 	printf("%u \n", pkt_get_crc1(decPakt));
+// 	pkt_del(paquet);
+// 	pkt_del(decPakt);
+//    	return 0;
+// }
--- sender.c
+++ sender.c
@@ -3,6 +3,93 @@
 #include <stddef.h> /* size_t */
 #include <stdint.h> /* uintx_t */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>  /* atoi */
+/*
+GROSSE ERREUR,
+prise en compte du cas ou on ne sait pas ou on est (premier_argument) seulement on le sait, puisque on est dans
+le main du sender ici ! 
+
+*/
+
+/* struct de argv : 
+			- si -f : sender -f fichier host numero_du_port
+			- sinon : receiver/sender host numero_du_port symbole fichier 
+			  avec rajout possible à la fin : 2> fichier message_d_erreur
+*/
+void connect(struct sockaddr_in6* si_other, char* hostname, int port){
+	//fill the socket struxture and connect to other socket
+	return;
+}
+
 int main(int argc, char* argv[]){
-	return 0;
+	int i;
+    int interpreter=1;
+    void *fileToRead=NULL, *hostname=NULL, *port=NULL;
+    for (i=1; i<argc; i++){ // manager exceptions : si arc =1 ou argc >5 ou pas -f er argc>3 on exit : nbr d'aruments trop faible
+        if(!strcmp(argv[i], "-f")){ //il faudra prendre en compte le "<"
+            i=i+1;
+            fileToRead=argv[i];
+            interpreter=0;
+        }else if(hostname==NULL){
+            hostname=argv[i];
+        }else if(port==NULL){     
+            port=argv[i];
+        }
+    }
+
+    printf("%s\n", hostname);
+    printf("%u\n", interpreter);
+    printf("%s\n", fileToRead);
+
+
+	// char* fichier;
+	// char* host;
+	// int port_number = 0;
+	// int premier_argument = 0; //permet de savoir s'il s'agit du receiver ou du sender
+	// int symbole = 0;
+	// char* erreur;
+	// if(strcmp(argv[0], "./sender") == 0){
+	// 	premier_argument = 1;
+	// }else if(strcmp(argv[0], "./receiver") == 0){
+	// 	premier_argument = 2;
+	// }else{
+	// 	fprintf(stderr, "le premier argument n'est ni un sender, ni un receiver \n");
+	// 	return -1;
+	// }
+
+	// if(strcmp(argv[1], "-f")==0){// on sait donc qu'il s'agit du sender, et que celui ci specifie l'arg d'apres le nom du fichier
+	// 	fichier = argv[2];
+	// 	host = argv[3];
+	// 	port_number = atoi(argv[4]); 
+	// 	if(port_number == 0){
+	// 		fprintf(stderr, "option -f : probleme au numero du port \n");
+	// 		return -1;
+	// 	}
+	// }else{
+	// 	host = argv[1];
+	// 	port_number = atoi(argv[2]);
+	// 	if(port_number == 0){
+	// 		fprintf(stderr, "option sans -f : probleme au numero du port \n");
+	// 		return -1;
+	// 	}
+
+	// 	if(argv[3] == "<"){
+	// 		symbole = 1;
+	// 	}else if(argv[3] == ">"){
+	// 		symbole = 2;
+	// 	}else{
+	// 		fprintf(stderr, "option sans -f : probleme au symbole \n");
+	// 		return -1;
+	// 	}
+	// 	fichier  = argv[4];
+	// 	if(argc>5){
+	// 		erreur = argv[6];
+	// 	}
+	// }
+
+
+	
 }
